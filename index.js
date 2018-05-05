87638788bb292b40f42dbadb5e6ce0a7bb54ead2
rubnet <1204075579@qq.com>
2018-05-05 10:23:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var selectAll = document.querySelectorAll(".div_question");
-for(var i = 1; i<selectAll.length -1 ; i++ ){
+for(var i = 1; i<selectAll.length+1 ; i++ ){
    var random = parseInt(Math.random()*(3-1)+1);
    var tmp = document.getElementById("divquestion" + i).querySelectorAll(".ulradiocheck li")[random-1];
-   tmp.querySelectorAll(".jqRadio")[0].click();
-}  
+   if( tmp.querySelectorAll(".jqRadio")[0])
+      tmp.querySelectorAll(".jqRadio")[0].click();
+   if(tmp.querySelectorAll(".jqCheckbox")[0])
+       tmp.querySelectorAll(".jqCheckbox")[0].click();
+}
